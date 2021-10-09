a067942f98248a2be3b5dc2ac3a53f64672452bd
Valinor13 <jwcalhoun2@gmail.com>
2021-10-08 23:06:28 -0500

    Finished product

--- scripts.js
+++ scripts.js
@@ -23,7 +23,66 @@ $(document).ready(function() {
     loadQuotes();
     loadPopularTutorials();
     loadLatestVideos();
+    loadAllVideos('', 'all', 'most_popular');
+    $('#searchSubmit').click(() => {
+      loadAllVideos($('#keyword').val(), $('#topic').val(), $('#sortOption').val());
+    })
 })
+
+function loadAllVideos(keyword, topic, sortOption) {
+  $.ajax({
+    url: `https://smileschool-api.hbtn.info/courses`,
+    type: 'GET',
+    q: keyword,
+    topic: topic,
+    sort: sortOption,
+    dataType: 'json',
+    jsonp: false,
+    beforeSend: function () {
+      $('.loader').show();
+    },
+    success: function(data) {
+      let cardArray = [];
+      $('#allVideos').empty();
+      if (topic === 'all') {
+          if (keyword === '') {
+            cardArray = data.courses;   
+          } else {
+            data.courses.forEach(element => {
+              element.keywords.forEach(item => {
+                if (keyword.toLowerCase().includes(item.toLowerCase())) {
+                  cardArray.push(element);
+                }
+              })
+            })
+          }
+      } else {
+          let topicCards = [];
+          data.courses.forEach(course => {
+            if (course.topic.toLowerCase() === topic) {
+              topicCards.push(course);
+            }
+          })
+          if (keyword === '') {
+            cardArray = topicCards;   
+          } else {
+            topicCards.forEach(element => {
+              element.keywords.forEach(item => {
+                if (keyword.toLowerCase().includes(item.toLowerCase())) {
+                  cardArray.push(element);
+                }
+              })
+            })
+          }
+      }
+      let finalArray = sortCards(cardArray, sortOption);
+      $(`#videoCount`).text(`${finalArray.length} videos`);
+      for (let i = 0; i < finalArray.length; i++) {
+        populateVids(finalArray[i], "#allVideos", "allVid");
+      }
+    }
+  })
+}
 
 function loadLatestVideos() {
   $.ajax({
@@ -37,29 +96,7 @@ function loadLatestVideos() {
     success: function(data) {
       $('#latestCarousel').empty();
       for (let i = 0; i < data.length; i++) {
-        $('#latestCarousel').append(`<div id="latestCard${i}" class="border-0 col-3 mx-3">`)
-        $(`#latestCard${i}`).append(`<img src=${data[i].thumb_url} height="auto" width="255px" alt="Card image cap #1">
-                                  <div class="p-2 pt-3">
-                                      <h6 class="font-weight-bolder">${data[i].title}</h6>
-                                      <p class="small text-muted">${data[i]["sub-title"]}</p>
-                                  </div>
-                                  <div class="border-0">
-                                      <div class="row m-0">
-                                          <img class="rounded-circle" src=${data[i].author_pic_url} width="30px" height="30px" alt="Review pic">
-                                          <p class="color-purp pl-3 pt-1">${data[i].author}</p>
-                                      </div>
-                                      <div class="row justify-content-between m-0">
-                                          <div id="latestStarReview${i}" class="row pl-2">
-                                          </div>
-                                          <p class="color-purp">${data[i].duration}</p>
-                                      </div>
-                                  </div>`);
-        for (let j = 0; j < data[i].star; j++) {
-          $(`#latestStarReview${i}`).append('<img src="/images/star_on.png" height="15px" width="15px" alt="star on">');
-        }
-        for (let j = 0; j < (5 - data[i].star); j++) {
-          $(`#latestStarReview${i}`).append('<img src="/images/star_off.png" height="15px" width="15px" alt="star off">');
-        }
+        populateVids(data[i], "#latestCarousel", "latest");
       }
     },
     complete: function () {
@@ -81,29 +118,7 @@ function loadPopularTutorials() {
     success: function(data) {
       $('#popCarousel').empty();
       for (let i = 0; i < data.length; i++) {
-        $('#popCarousel').append(`<div id="popCard${i}" class="border-0 col-3 mx-3">`)
-        $(`#popCard${i}`).append(`<img src=${data[i].thumb_url} height="auto" width="255px" alt="Card image cap #1">
-                                  <div class="p-2 pt-3">
-                                      <h6 class="font-weight-bolder">${data[i].title}</h6>
-                                      <p class="small text-muted">${data[i]["sub-title"]}</p>
-                                  </div>
-                                  <div class="border-0">
-                                      <div class="row m-0">
-                                          <img class="rounded-circle" src=${data[i].author_pic_url} width="30px" height="30px" alt="Review pic">
-                                          <p class="color-purp pl-3 pt-1">${data[i].author}</p>
-                                      </div>
-                                      <div class="row justify-content-between m-0">
-                                          <div id="popStarReview${i}" class="row pl-2">
-                                          </div>
-                                          <p class="color-purp">${data[i].duration}</p>
-                                      </div>
-                                  </div>`);
-        for (let j = 0; j < data[i].star; j++) {
-          $(`#popStarReview${i}`).append('<img src="/images/star_on.png" height="15px" width="15px" alt="star on">');
-        }
-        for (let j = 0; j < (5 - data[i].star); j++) {
-          $(`#popStarReview${i}`).append('<img src="/images/star_off.png" height="15px" width="15px" alt="star off">');
-        }
+        populateVids(data[i], "#popCarousel", "pop");
       }
     },
     complete: function () {
@@ -125,9 +140,9 @@ function loadQuotes() {
     success: function(data) {
       $('.single-item').empty();
       for (let i = 0; i < data.length; i++) {
-        $('.single-item').append(`<div id="quotes${i}" class="col-8 mx-auto">`);
-        $(`#quotes${i}`).append(`<div id="quotesCard${i}" class="text-white d-sm-flex flex-sm-row">`);
-        $(`#quotesCard${i}`).append(`<img src="${data[i].pic_url}" class="d-block rounded-circle mt-110 mx-auto ml-sm-5 mx-sm-none" width="150px" height="150px" alt="Profile Pic #5">
+        $('.single-item').append(`<div id="quotes${data[i].id}" class="col-8 mx-auto">`);
+        $(`#quotes${data[i].id}`).append(`<div id="quotesCard${data[i].id}" class="text-white d-sm-flex flex-sm-row">`);
+        $(`#quotesCard${data[i].id}`).append(`<img src="${data[i].pic_url}" class="d-block rounded-circle mt-110 mx-auto ml-sm-5 mx-sm-none" width="150px" height="150px" alt="Profile Pic #5">
                                       <div class="mb-110 mt-110 px-5 cara-cust">
                                         <p>« ${data[i].text}</p>
                                         <h5>${data[i].name}</h5>
@@ -140,4 +155,75 @@ function loadQuotes() {
       $('.single-item').slick();
     }
   })
+}
+
+function populateVids(card, outerDiv, innerDiv) {
+  $(outerDiv).append(`<div id="${innerDiv}Card${card.id}" class="border-0 col-3 mx-3 pb-5">`);
+  $(`#${innerDiv}Card${card.id}`).append(`<img src=${card.thumb_url} height="auto" width="255px" alt="Card image cap #1">
+                                      <div class="p-2 pt-3">
+                                          <h6 class="font-weight-bolder">${card.title}</h6>
+                                          <p class="small text-muted">${card["sub-title"]}</p>
+                                      </div>
+                                      <div class="border-0">
+                                          <div class="row m-0">
+                                              <img class="rounded-circle" src=${card.author_pic_url} width="30px" height="30px" alt="Review pic">
+                                              <p class="color-purp pl-3 pt-1">${card.author}</p>
+                                          </div>
+                                          <div class="row justify-content-between m-0">
+                                              <div id="${innerDiv}StarReview${card.id}" class="row pl-2">
+                                              </div>
+                                              <p class="color-purp">${card.duration}</p>
+                                          </div>
+                                      </div>`);
+        for (let j = 0; j < card.star; j++) {
+          $(`#${innerDiv}StarReview${card.id}`).append('<img src="/images/star_on.png" height="15px" width="15px" alt="star on">');
+        }
+        for (let j = 0; j < (5 - card.star); j++) {
+          $(`#${innerDiv}StarReview${card.id}`).append('<img src="/images/star_off.png" height="15px" width="15px" alt="star off">');
+        }
+}
+
+function sortCards(cardArray, sortOption) {
+  let finalArray = [];
+  if (sortOption === 'most_popular') {
+    let optionArray = cardArray.map(element => {
+      return (element.star); 
+    })
+    optionArray.sort(function(a, b){return b-a});
+    let optionSet = new Set(optionArray);
+    [...optionSet].forEach(item => {
+      cardArray.forEach(card => {
+        if (card.star === item) {
+          finalArray.push(card);
+        }
+      })
+    })
+  } else if (sortOption === 'most_viewed') {
+      let optionArray = cardArray.map(element => {
+        return (element.views);
+      })
+      optionArray.sort(function(a, b){return b-a});
+      let optionSet = new Set(optionArray);
+      [...optionSet].forEach(item => {
+        cardArray.forEach(card => {
+          if (card.views === item) {
+            finalArray.push(card);
+          }
+        })
+      })
+  } else {
+    let optionArray = cardArray.map(element => {
+      return (element.published_at);
+    })
+    optionArray.sort(function(a, b){return b-a});
+    let optionSet = new Set(optionArray);
+    [...optionSet].forEach(item => {
+      cardArray.forEach(card => {
+        if (card.published_at === item) {
+          finalArray.push(card);
+        }
+      })
+    })
+  }
+  return (finalArray);
 }
